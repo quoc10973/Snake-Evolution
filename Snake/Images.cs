41c2ea690b1fed516c7bd933deceff946ad73b72
quoc10973 <rumble01667057271@gmail.com>
2024-12-03 15:56:07 +0700

    images loader

---
+++ Snake/Images.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+
+namespace Snake
+{
+    public static class Images
+    {
+        public readonly static ImageSource Empty = LoadImage("Empty");      
+        public readonly static ImageSource Head = LoadImage("Head");
+        public readonly static ImageSource Tail = LoadImage("Body");
+        public readonly static ImageSource Food = LoadImage("Food");
+        public readonly static ImageSource Wall = LoadImage("DeadBody");
+        public readonly static ImageSource Snake = LoadImage("DeadHead");
+
+        private static ImageSource LoadImage(string fileName)
+        {
+            return new BitmapImage(new Uri($"Assets/{fileName}.png", UriKind.Relative));
+        }
+    }
+}
